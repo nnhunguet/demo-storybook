b911519a4c2b6c4cb8188f0cee9ad83f4f9e4b36
nnhunguet <52577080+nnhunguet@users.noreply.github.com>
2020-06-08 15:40:24 +0700

    111111

---
+++ .out/precache-manifest.90e87adbd7ff0eaccfed1fa7c07596b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d3d16087f5f5fc0b375f47e13069049",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.e711f7e8f87593467698.bundle.js"
+  },
+  {
+    "url": "runtime~main.e711f7e8f87593467698.bundle.js"
+  },
+  {
+    "revision": "2223c4224b533e066022",
+    "url": "static/css/main.1c07a30f.chunk.css"
+  },
+  {
+    "revision": "43aadce86b9b819bde3e",
+    "url": "static/css/vendors~main.f8f248a5.chunk.css"
+  },
+  {
+    "url": "vendors~main.e711f7e8f87593467698.bundle.js"
+  },
+  {
+    "url": "vendors~main.e711f7e8f87593467698.bundle.js.LICENSE.txt"
+  }
+]);
