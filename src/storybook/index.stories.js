4e6cf939091b23f5840ad1d13a5a0318ed1c6703
nnhunguet <52577080+nnhunguet@users.noreply.github.com>
2020-06-08 15:45:50 +0700

    222

--- src/storybook/index.stories.js
+++ src/storybook/index.stories.js
@@ -8,7 +8,7 @@ storiesOf('FaceBook', module)
     'default',
     () => (
       <FaceBook
-          title="Tây Tiến"
+          title="Tây Tiến 111"
           desc="Tác phẩm Tây Tiến"
           imageURL="https://res.cloudinary.com/nnhungcoderx/image/upload/v1591289041/kmfgbgrpvxc4xiuwcdlc.jpg"
       />
